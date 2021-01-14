af1f646707e3e8b04209d4a376c10caa7b19a333
Samuel Trigger <samuel.j.trigger@gmail.com>
2021-01-14 21:09:40 +1000

    Updates

---
+++ precache-manifest.2df2a37c998566de9328a7d478ddd41b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5372f94c22677d6e12f4c6d0e34191b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ab6e7fa09d9245548f4",
+    "url": "/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "da6fd7a02cd995498cb5",
+    "url": "/static/js/2.8e73fc4f.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.8e73fc4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ab6e7fa09d9245548f4",
+    "url": "/static/js/main.2a73a896.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/static/media/resume.9d3da0e2.pdf"
+  }
+]);
