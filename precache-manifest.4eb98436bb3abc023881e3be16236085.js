4c19773b66c420d3ca8d72f2cf1a92925b6fdfc9
Samuel Trigger <samuel.j.trigger@gmail.com>
2021-01-14 21:40:19 +1000

    Updates

---
+++ precache-manifest.4eb98436bb3abc023881e3be16236085.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eece26fde5bfae40e56d98030d780113",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ddad8a5bf978a196a98b",
+    "url": "/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "09c8ef8dc029182e5c1e",
+    "url": "/static/js/2.bd2b4a3e.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.bd2b4a3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddad8a5bf978a196a98b",
+    "url": "/static/js/main.d8b5e32b.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/static/media/resume.9d3da0e2.pdf"
+  },
+  {
+    "revision": "4abb332c14add4586f1623c6c626b87f",
+    "url": "/static/media/samueltrigger.4abb332c.jpg"
+  }
+]);
